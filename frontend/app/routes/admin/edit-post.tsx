b458b735e2502b6945258688fe9380e0ae62e54b
Lucon05 <luiscontrerasmarquez2611@gmail.com>
2026-03-24 18:05:19 +0100

    admind acciones

--- frontend/app/routes/admin/edit-post.tsx
+++ frontend/app/routes/admin/edit-post.tsx
@@ -1,11 +1,117 @@
+import { useEffect, useState } from "react";
+import { Checkbox, FormControlLabel } from "@mui/material";
+import { useNavigate } from "react-router";
 
+import type { Route } from "./+types/edit-post";
+import Button from "~/components/Button";
+import Input from "~/components/Input";
+import { Textarea } from "~/components/textarea";
+import { deletePost, fetchPost, putPost } from "../blog/post";
 
-export default function EditPost(){
-    
-return(
-    <div>
-        <h1> Edit Post</h1>
-            </div>
-)
+export default function EditPost({ params: { slug } }: Route.ComponentProps) {
+  const [postData, setPostData] = useState({
+    title: "",
+    body: "",
+    published: false,
+  });
+  const navigate = useNavigate();
+  const [submitting, setSubmitting] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    setError(null);
+    fetchPost(slug)
+      .then((post) => {
+        setPostData(post);
+      })
+      .catch((err: Error) => {
+        setError(err.message);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [slug]);
+
+  if (loading) {
+    return "Cargando...";
+  }
+
+  async function savePost() {
+    try {
+      setError(null);
+      setSubmitting(true);
+      const res = await putPost(postData, slug);
+      await navigate("/admin/edit-post/" + res.slug, { replace: true });
+    } catch (err) {
+      if (err instanceof Error) {
+        setError(err.message);
+      }
+
+      console.error(err);
+    } finally {
+      setSubmitting(false);
+    }
+  }
+
+  async function removePost() {
+    const confirmed = confirm(
+      "Estas seguro de que quieres eliminarlo? Esta accion es irreversible",
+    );
+
+    if (!confirmed) return;
+
+    try {
+      setError(null);
+      setSubmitting(true);
+      await deletePost(slug);
+      await navigate("/admin");
+    } catch (err) {
+      if (err instanceof Error) {
+        setError(err.message);
+      }
+
+      console.error(err);
+    } finally {
+      setSubmitting(false);
+    }
+  }
+
+  return (
+    <div className="flex flex-col gap-4 p-4">
+      <h1>Edit Post</h1>
+      <Input
+        value={postData.title}
+        onChange={(event) =>
+          setPostData({ ...postData, title: event.target.value })
+        }
+        label="Title:"
+      ></Input>
+      <Textarea
+        value={postData.body}
+        onChange={(event) =>
+          setPostData({ ...postData, body: event.target.value })
+        }
+        label="Body:"
+      ></Textarea>
+      <FormControlLabel
+        control={
+          <Checkbox
+            checked={postData.published}
+            onChange={(event) =>
+              setPostData({ ...postData, published: event.target.checked })
+            }
+          />
+        }
+        label="Published"
+      />
+      {error && <span className="text-red-500">Error: {error}</span>}
+      <Button onClick={savePost} disabled={submitting}>
+        Save
+      </Button>
+      <Button onClick={removePost} disabled={submitting} variant="danger">
+        Delete
+      </Button>
+    </div>
+  );
 }
-
--- frontend/app/routes/admin/new-post.tsx
+++ frontend/app/routes/admin/new-post.tsx
@@ -1,26 +1,70 @@
-import { useState } from "react"
-import z from "zod"
+import { useState } from "react";
+import { Checkbox, FormControlLabel } from "@mui/material";
+import { useNavigate } from "react-router";
+
+import Button from "~/components/Button";
 import Input from "~/components/Input";
 import { Textarea } from "~/components/textarea";
+import { createPost } from "../blog/post";
 
-const postSchema = z.object({
-  title: z.string(),
-  body: z.string(),
-  slug: z.string(),
-});
+export default function PutNewPost() {
+  const [postData, setPostData] = useState({ // se desectrustura el post data 
+    title: "",
+    body: "",
+    published: false,
+  });
+  const navigate = useNavigate();                           //aqui hay 3 variables  una para navegar
+  const [submitting, setSubmitting] = useState(false);   //una para saber si el post se a subido o slo se guardo  pero mas adelante se hace una funcioon  que es savePost  //el usstates falso  porque despues puede cambiar de estado  si se publica o no en el checkbox
+  const [error, setError] = useState<string | null>(null);    // errors
 
+  async function savePost() {
+    try {
+      setSubmitting(true);
+      const res = await createPost(postData);
+      await navigate("/admin/edit-post/" + res.slug, { replace: true });
+    } catch (err) {
+      if (err instanceof Error) {
+        setError(err.message);
+      }
 
-export default function NewPost(){
-      const [incertPost, setIncertPost] = useState(postSchema);
-    
-    useState
-return(
-    <div>
-        <h1>New Post</h1>
-        <Input>titulo</Input>
-        <Textarea></Textarea>
-        
+      console.error(err);
+    } finally {
+      setSubmitting(false);
+    }
+  }
+
+  return (
+    <div className="flex flex-col gap-4 p-4">
+      <h1>New Post</h1>
+      <Input
+        value={postData.title}
+        onChange={(event) =>
+          setPostData({ ...postData, title: event.target.value })
+        }
+        label="Title:"
+      ></Input>
+      <Textarea
+        value={postData.body}
+        onChange={(event) =>
+          setPostData({ ...postData, body: event.target.value })
+        }
+        label="Body:"
+      ></Textarea>
+      <FormControlLabel
+        control={
+          <Checkbox
+            checked={postData.published}
+            onChange={(event) =>
+              setPostData({ ...postData, published: event.target.checked })
+            }
+          />
+        }
+        label="Published"
+      />
+      {error && <span className="text-red-500">Error: {error}</span>}
+      <Button onClick={savePost} disabled={submitting}>
+        Save
+      </Button>
     </div>
-)
+  );
 }
-
