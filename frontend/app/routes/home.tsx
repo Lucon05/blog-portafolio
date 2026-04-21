4150fa115b2ba59e85d1a437e37905d0e8162d0f
Lucon05 <luiscontrerasmarquez2611@gmail.com>
2026-04-22 00:56:56 +0200

    recreando home

--- frontend/app/routes/home.tsx
+++ frontend/app/routes/home.tsx
@@ -1,6 +1,5 @@
 import type { Route } from "./+types/home";
 import Cartica from "~/components/carticas";
-import Footer from "~/components/footer";
 import Menu from "~/components/menu";
 
 export function meta(_metaArgs: Route.MetaArgs) {
@@ -9,52 +8,58 @@ export function meta(_metaArgs: Route.MetaArgs) {
 
 export default function Home() {
   return (
-    <div className="bottom-0">
-      <div className="flex flex-wrap">
-        <div className="w-2/12">
+    <div className="">
+      <div className="">
+        <div className="">
           <Menu
-            MenuContenido1={" Über mich"}
+            MenuContenido1={" Über mich "}
             MCW1={""}
             MenuContenido2={" Projekte "}
             MCW2={""}
-            MenuContenido3={" Lebenslauf "}
-            MCW3={""}
-            MenuContenido4={" Sprachen "}
-            MCW4={""}
             MenuContenido5={" Arbeitserfahrung "}
             MCW5={""}
           />
         </div>
 
-        <div className="w-10/12 flex-col">
-          <div className="flex w-10/10 flex-col items-end p-10">
-            <h2 className="py-5 text-2xl font-bold text-[#545454] sm:text-3xl md:text-4xl lg:text-5xl">
+        <div className="">
+          <div className="m-5 p-5">
+            <h2 className="text-center text-2xl">
               <b>Hallo ich bin Luis</b>
             </h2>
-            <div className="flex gap-2 sm:flex-col">
+            <p className="mt-5 text-center">
+              Si cum multis loqui vis, pluribus linguis tibi discenda est.
+              Linguae internationales magni momenti sunt, quia multi homines
+              terrarum diversarum in orbe terrarum sunt. Sunt plus quam septem
+              milia linguarum in orbe terrarum. Lingua non solum modus est
+              communicationis, linguae mundos aperit. Lingua Arabica, hispanica,
+              sinica, lusitanica, anglica maximi moment sunt. Multi in America
+              Foederata...
+            </p>
+          </div>
+            <div className="">
               <Cartica
                 className=""
                 carticaLink={""}
                 photo={
                   "https://cdn-ilddihb.nitrocdn.com/MgqZCGPEMHvMRLsisMUCAIMWvgGMxqaj/assets/images/optimized/rev-b472ba7/www.goya.com/wp-content/uploads/2023/10/arepas-cornmeal-patties.jpg"
                 }
-                textCartica="caramoco dembele"
-                title="lolalolita"
+                textCartica="Wie sah mein IT-Alltag auf der Insel aus? Ein Rückblick auf meine täglichen Wege und Erlebnisse"
+                title="Auslandspraktikum"
               ></Cartica>
-              <p className="text-1l sm:t md:text-1xl max-w-7/12 text-right text-[#545454] lg:text-3xl">
-                Si cum multis loqui vis, pluribus linguis tibi discenda est.
-                Linguae internationales magni momenti sunt, quia multi homines
-                terrarum diversarum in orbe terrarum sunt. Sunt plus quam septem
-                milia linguarum in orbe terrarum. Lingua non solum modus est
-                communicationis, linguae mundos aperit. Lingua Arabica,
-                hispanica, sinica, lusitanica, anglica maximi moment sunt. Multi
-                in America Foederata...
-              </p>
             </div>
-          </div>
-          <div className="mb-10 flex flex-wrap justify-end gap-8 sm:flex-col">
+          
+          <div className="">
             <Cartica
-              className="max-w-5/12"
+              className=""
+              carticaLink={""}
+              photo={
+                "https://cdn-ilddihb.nitrocdn.com/MgqZCGPEMHvMRLsisMUCAIMWvgGMxqaj/assets/images/optimized/rev-b472ba7/www.goya.com/wp-content/uploads/2023/10/arepas-cornmeal-patties.jpg"
+              }
+              textCartica="caramoco dembele"
+              title="Auslandpracktikum"
+            ></Cartica>
+            <Cartica
+              className=""
               carticaLink={""}
               photo={
                 "https://cdn-ilddihb.nitrocdn.com/MgqZCGPEMHvMRLsisMUCAIMWvgGMxqaj/assets/images/optimized/rev-b472ba7/www.goya.com/wp-content/uploads/2023/10/arepas-cornmeal-patties.jpg"
@@ -62,15 +67,7 @@ export default function Home() {
               textCartica="caramoco dembele"
               title="lolalolita"
             ></Cartica>
-            <Cartica
-              className="mr-16 max-w-4/12"
-              carticaLink={""}
-              photo={
-                "https://cdn-ilddihb.nitrocdn.com/MgqZCGPEMHvMRLsisMUCAIMWvgGMxqaj/assets/images/optimized/rev-b472ba7/www.goya.com/wp-content/uploads/2023/10/arepas-cornmeal-patties.jpg"
-              }
-              textCartica="caramoco dembele"
-              title="lolalolita"
-            ></Cartica>
+            
           </div>
         </div>
       </div>
