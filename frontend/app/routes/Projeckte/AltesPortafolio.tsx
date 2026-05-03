7330c883432167d31f9d11c6cdb13abbbccec7c0
Lucon05 <luiscontrerasmarquez2611@gmail.com>
2026-05-03 19:37:35 +0200

    altesportafoliofinito

--- frontend/app/routes/Projeckte/AltesPortafolio.tsx
+++ frontend/app/routes/Projeckte/AltesPortafolio.tsx
@@ -16,53 +16,56 @@ export default function AltesPortafolio() {
         </Link>
       </div>
       <div>
-        <div className="flex-row-reverse lg:mt-20  lg:flex lg:items-center">
-          <div className=" m-5 p-5 lg:max-w-4/8">
+        <div className="text-[#2D2D2D] flex-row-reverse lg:mt-20 lg:flex lg:items-center">
+          <div className="m-5 p-5 lg:max-w-4/8">
             <h2 className="text-center text-2xl sm:text-4xl md:text-left md:text-5xl lg:text-6xl">
               <b>Projektbeschreibung: </b>
-
             </h2>
-                          <p className=" mt-5 text-center sm:text-2xl md:text-left md:text-3xl lg:text-2xl xl:text-4xl">
-                Dieses Projekt war mein erstes Portfolio, das ich während meiner
-                Zeit bei der Azubi Digital Akademie erstellt habe. Damals habe
-                ich gerade erst angefangen, HTML und CSS zu lernen. Ich habe die
-                Seite komplett selbst aufgebaut, um ein Gefühl dafür zu
-                bekommen, wie Webseiten strukturiert und gestaltet werden.e, um
-                das Display testen zu können.
-              </p>
-              <p className=" mt-5 text-center sm:text-2xl md:text-left md:text-3xl lg:text-2xl xl:text-4xl">
-              <b>Status::</b> Abgeschlossen
+            <p className="mt-5 text-center sm:text-2xl md:text-left md:text-3xl lg:text-2xl xl:text-4xl">
+              Dieses Projekt war mein erstes Portfolio, das ich während meiner
+              Zeit bei der Azubi Digital Akademie erstellt habe. Damals habe ich
+              gerade erst angefangen, HTML und CSS zu lernen. Ich habe die Seite
+              komplett selbst aufgebaut, um ein Gefühl dafür zu bekommen, wie
+              Webseiten strukturiert und gestaltet werden. <a className=" text-center hover:text-[#822a34d3] sm:text-2xl md:text-left md:text-3xl lg:text-2xl xl:text-4xl"
+              href="https://lucon05.github.io/web-Portafolio/index.html"><b>zum Alten Portfolio</b></a>
+            </p>
+            <p className="mt-5 text-center sm:text-2xl md:text-left md:text-3xl lg:text-2xl xl:text-4xl">
+              <b>Status: </b>Abgeschlossen
             </p>
           </div>
           <div className="">
             <Cartica
               className=""
-              carticaLink={""}
+              carticaLink="https://lucon05.github.io/web-Portafolio/index.html"
               photo={"Img-Portafolio/Projekte/ImgOldCode.png"}
             ></Cartica>
           </div>
         </div>
-        <div className="lg:mb-20 lg:flex lg:items-center">
+        <div className=" text-[#2D2D2D] lg:mb-20 lg:flex lg:items-center">
           <div className="lg m-5 p-5 lg:max-w-4/8">
             <h2 className="text-center text-2xl sm:text-4xl md:text-left md:text-5xl lg:text-6xl">
               <b>Was ich dabei gelernt habe: </b>
-              </h2>
-              <p className="mt-5 text-center sm:text-2xl md:text-left md:text-3xl lg:text-2xl xl:text-4xl">Grundlagen von HTML und CSS</p>
-              <p className="mt-5 text-center sm:text-2xl md:text-left md:text-3xl lg:text-2xl xl:text-4xl">Erste Layouts und Styling</p>
+            </h2>
+            <p className="mt-5 text-center sm:text-2xl md:text-left md:text-3xl lg:text-2xl xl:text-4xl">
+              Grundlagen von HTML und CSS
+            </p>
+            <p className="mt-5 text-center sm:text-2xl md:text-left md:text-3xl lg:text-2xl xl:text-4xl">
+              Erste Layouts und Styling
+            </p>
             <h2 className="mt-10 text-center text-2xl sm:text-4xl md:text-left md:text-5xl lg:text-6xl">
               <b>Rückblick</b>
-              </h2>
-              <p className="mt-5 text-center sm:text-2xl md:text-left md:text-3xl lg:text-2xl xl:text-4xl">Rückblickend enthält das Projekt noch viele Fehler und ist nicht responsiv. Trotzdem ist es für mich ein wichtiger Meilenstein, weil ich hier meine ersten praktischen Erfahrungen gesammelt habe.</p>
-      
-            
-
-            
+            </h2>
+            <p className="mt-5 text-center sm:text-2xl md:text-left md:text-3xl lg:text-2xl xl:text-4xl">
+              Rückblickend enthält das Projekt noch viele Fehler und ist nicht
+              responsiv. Trotzdem ist es für mich ein wichtiger Meilenstein,
+              weil ich hier meine ersten praktischen Erfahrungen gesammelt habe.
+            </p>
           </div>
           <div className="">
             <Cartica
               className=""
-              carticaLink={""}
               photo={"Img-Portafolio/Projekte/ImgOldPortafolio.png"}
+              carticaLink="https://lucon05.github.io/web-Portafolio/index.html"
             ></Cartica>
           </div>
         </div>
